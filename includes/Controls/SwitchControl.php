f074f861913fa59239fd7317247515eee1fa438e
aristath <aristath@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 19:20:01 +0000

    Adding 0.8.2 tag
    git-svn-id: https://plugins.svn.wordpress.org/kirki/tags/0.8.2@1123841 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/Controls/SwitchControl.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kirki\Controls;
+
+use Kirki;
+
+class SwitchControl extends \WP_Customize_Control {
+
+	public $type = 'switch';
+
+	/**
+	 * Render the control's content.
+	 */
+	protected function render_content() { ?>
+		<?php $i18n = Kirki::i18n(); ?>
+		<label>
+			<div class="switch-info">
+				<input style="display: none;" type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+			</div>
+			<?php echo esc_html( $this->label ); ?>
+			<?php if ( ! empty( $this->description ) ) : ?>
+				<span class="description customize-control-description"><?php echo $this->description; ?></span>
+			<?php endif; ?>
+			<?php $classes = ( esc_attr( $this->value() ) ) ? ' On' : ' Off'; ?>
+			<div class="Switch <?php echo $classes; ?>">
+				<div class="Toggle"></div>
+				<span class="On"><?php echo $i18n['ON']; ?></span>
+				<span class="Off"><?php echo $i18n['OFF']; ?></span>
+			</div>
+		</label>
+		<?php
+	}
+}
