de981c5feff80adc28ac1154842af9bee980c2a5
aristath <aristath@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 18:45:01 +0000

    partial commit for 0.8.1
    git-svn-id: https://plugins.svn.wordpress.org/kirki/tags/0.8.1@1123818 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kirki.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name:   Kirki Framework
+Plugin URI:    http://kirki.org
+Description:   An options framework using and extending the WordPress Customizer
+Author:        Aristeides Stathopoulos
+Author URI:    http://press.codes
+Version:       0.8.1
+Text Domain:   kirki
+*/
+
+if ( ! defined( 'KIRKI_PATH' ) ) {
+	define( 'KIRKI_PATH', dirname( __FILE__ ) );
+}
+if ( ! defined( 'KIRKI_URL' ) ) {
+	define( 'KIRKI_URL', plugin_dir_url( __FILE__ ) );
+}
+
+// Include the main kirki class
+include_once( KIRKI_PATH . '/includes/Kirki.php' );
+
+// Make sure the class is instanciated
+Kirki::get_instance();
